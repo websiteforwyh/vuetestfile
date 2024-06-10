dff252cb7fae19722b48831b5911b9895479a31a
gitwyh <2085195880@qq.com>
2024-06-10 17:21:16 +0800

    1

--- src/views/side/VueView.vue
+++ src/views/side/VueView.vue
@@ -10,8 +10,7 @@
         <li>基本环境配置</li>
         <li>1、进入官网下载node.js</li>
         <li>官网链接：<a href="https://nodejs.org/en/">https://nodejs.org/en/</a></li>
-        <img :src="require('@/assets/VueNote/node.png')" alt="Description"
-         width="780px" height="500px">
+        <img :src="require('@/assets/VueNote/node.png')" alt="Description" width="780px" height="500px">
         <li>下载后打开安装软件无脑装即可</li>
         <li>2、安装好Node.js后终端(cmd)运行输入<span>npm install -g @vue/cli</span>，这是全局安装vue指令的办法</li>
         <li>也可简易输入<span>npm install vue</span></li>
@@ -20,8 +19,7 @@
         <li>进入该目录（vue运行指令文件的存放位置）</li>
         <img :src="require('@/assets/VueNote/vuepathconfig.png')" alt="Description">
         <li>将地址复制下来保存到环境变量path中，记得全选确定</li>
-        <img :src="require('@/assets/VueNote/environmentconfig.png')" alt="Description"
-        width="780px" height="500px">
+        <img :src="require('@/assets/VueNote/environmentconfig.png')" alt="Description" width="780px" height="500px">
         <li>3、配置好vue指令后开始创建vue脚手架：<span>vue create filename</span></li>
         <li>下图说明要创建的文件名已存在，给出三个选项 覆盖文件，合并文件，取消创建，键盘箭头按键选择，Enter确定</li>
         <img :src="require('@/assets/VueNote/directoryexist.png')" alt="Description">
@@ -33,7 +31,8 @@
         <img :src="require('@/assets/VueNote/version.png')" alt="Description">
         <li>一些选项</li>
         <img :src="require('@/assets/VueNote/optiondisplay.png')" alt="Description" width="800px">
-        <li>vue脚手架创建完毕如下, 第一个为进入目录，第二个为运行服务，<red>一定要进入目录才能运行服务！！！</red></li>
+        <li>vue脚手架创建完毕如下, 第一个为进入目录，第二个为运行服务，<red>一定要进入目录才能运行服务！！！</red>
+        </li>
         <img :src="require('@/assets/VueNote/vuecreated.png')" alt="Description" width="900px">
         <li>如果没有进入目录而运行<span>npm run serve</span>，就会报错如下</li>
         <img :src="require('@/assets/VueNote/runserveerror.png')" alt="Description" width="900px">
@@ -55,11 +54,11 @@ span {
   background-color: yellowgreen;
 }
 
-red{
+red {
   color: red;
 }
 
-img{
+img {
   width: 900px;
 }
 </style>
